040ddd64afcdbb1d236e141a99e88e1d5b68e8f7
Fatalis <connman2400@gmail.com>
2022-12-20 17:59:24 -0600

    added comment module

---
+++ models/Comments.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends module{}
+
+Comments.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      comment_text:{
+        type: DataTypes.STRING,
+        allowNull:false,
+      },
+      user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model:'user',
+            key:'id'
+        }
+      }
+},{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+  });
+
+  module.exports = Comments;
