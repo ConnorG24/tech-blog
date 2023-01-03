0e7a11a4a99dc26d20c17a63c290cccf073c5640
Fatalis <connman2400@gmail.com>
2023-01-03 05:52:25 -0600

    index for the api folder

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const commentRoute = require('./commentRoute');
+const postRoute = require('./postRoute');
+const userRoute = require('./userRoute');
+
+router.use('/comments',commentRoute);
+router.use('/post',postRoute);
+router.use('/users',userRoute);
