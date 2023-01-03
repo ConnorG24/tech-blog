d5e31f83be011e7e7a907745d35c46c2c19abde4
Fatalis <connman2400@gmail.com>
2023-01-03 05:47:45 -0600

    post route finished

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -23,3 +23,61 @@ router.get('/:id', async (req,res) =>{
         res.status(500).json(err);
     }
 })
+
+// create a post
+
+router.post('/',withAuth,async (req,res) =>{
+    try{
+        
+        Post.create({
+            title: req.body.title,
+            content: req.body.title,
+            user_id: req.body.user_id
+        }).then((postData) =>{
+            res.json(postData)
+        })
+    
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+// update/ change post
+
+router.put('/:id', async (req,res) =>{
+    try{
+        Post.update({
+            title: req.body.title,
+            content: req.body.content
+        },{
+            where:{
+                id: req.params.id
+            }
+        }).then((postData) =>{
+            if(!postData){
+                res.status(404).json({message: 'post not found kupo'})
+            }
+            res.json(postData)
+        })
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+//delete post
+
+router.delete('/:id', async (req,res) =>{
+    try{
+        Post.destroy({
+            where:{
+                id: req.params.id 
+            }
+        }).then((postData) =>{
+            if(!postData){
+                res.status(404).json({message: 'post not found kupo'})
+            }
+            res.json(postData)
+        })
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
